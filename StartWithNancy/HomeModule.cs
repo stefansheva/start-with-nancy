80d99504cba3a864291f7c037906958aca374d24
sheva <stefan@codechem.com>
2020-01-30 15:25:40 +0100

    initial commit

---
+++ StartWithNancy/HomeModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nancy;
+
+namespace StartWithNancy
+{
+    public class HomeModule: NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = p => "Hello World";
+            Get["/show-html"] = p => View["index.html"];
+        }
+    }
+}
